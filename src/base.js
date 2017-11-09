012f0432db417e27602f4b5ca62e40e8a087eb03
andela-odurodola <Oluwadamilola.durodola@andela.com>
2017-11-09 13:40:14 +0100

    React app

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAP1JZFZsKgZZRpSTGq17bsUFmylXOxusk",
+  authDomain: "catch-of-the-day-dee.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-dee.firebaseio.com",
+});
+
+export default base;
